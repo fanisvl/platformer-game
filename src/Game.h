be811f52aff65814205ba71cdc87429c402bbfca
fanisvl <fanis.vlahogiannis@gmail.com>
2023-12-22 22:13:25 +0200

    init

---
+++ src/Game.h
@@ -0,0 +1,13 @@
+#ifndef PLATFORMER_GAME_GAME_H
+#define PLATFORMER_GAME_GAME_H
+
+class Game {
+public:
+    void update();
+    void draw();
+    void init();
+    Game();
+    ~Game();
+};
+
+#endif //PLATFORMER_GAME_GAME_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,32 @@
-#include <iostream>
+#include "../sgg/include/graphics.h"
+#include "Game.h"
+
+void update(float ms) {
+    Game* game = graphics::getUserData();
+}
+
+void draw() {
+
+}
+
+int main() {
+
+    // Window
+    graphics::createWindow(1200, 600, "Tutorial");
+    graphics::setCanvasSize(1000, 500);
+    graphics::setCanvasScaleMode(graphics::CANVAS_SCALE_FIT);
+
+    // Draw & Update setup
+    graphics::setDrawFunction(draw);
+    graphics::setUpdateFunction(update);
+
+    // Game object
+    Game game;
+    graphics::setUserData(&game);
+    game.init();
+
+    // Library starts calling update & draw
+    graphics::startMessageLoop();
+
+    return 0;
+}

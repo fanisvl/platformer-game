f52822e157a3207a95bd3e989aa21535893e42f5
fanisvl <fanis.vlahogiannis@gmail.com>
2023-12-26 19:53:44 +0200

    add comment for asset path

--- include/config.h
+++ include/config.h
@@ -1,5 +1,7 @@
 #pragma once
 
+// Asset path is relative to where the project is ran from.
+// This asset path assumes that you execute from the top project folder.
 #define ASSET_PATH "assets/"
 #define WINDOW_WIDTH 1200
 #define WINDOW_HEIGHT 600

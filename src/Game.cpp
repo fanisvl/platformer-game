051f523b931852794cbcbc76e7b2097e23622e51
fanisvl <fanis.vlahogiannis@gmail.com>
2023-12-26 20:48:38 +0200

    fix endless new player bug

--- src/Game.cpp
+++ src/Game.cpp
@@ -4,15 +4,27 @@
 #include "../sgg/include/graphics.h"
 
 void Game::update() {
+
+    if (!player) {
+        player = new Player();
+    }
+
+    if (player) {
+        player->update();
+    }
 }
 
 void Game::draw() {
+
+    // Draw Background
     graphics::Brush br;
     br.texture = std::string(ASSET_PATH) + "city_background.png";
+    graphics::drawRect(CANVAS_WIDTH/2, CANVAS_HEIGHT/2, CANVAS_WIDTH, CANVAS_HEIGHT, br);
 
-    float bgOffset = CANVAS_WIDTH * sinf(graphics::getGlobalTime()/1000.0f) / 4;
-
-    graphics::drawRect(CANVAS_WIDTH/2 + bgOffset, CANVAS_HEIGHT/2, CANVAS_WIDTH, CANVAS_HEIGHT, br);
+    // Draw Player
+    if (player) {
+        player->draw();
+    }
 }
 
 
@@ -23,5 +35,7 @@ Game::Game() {
 
 };
 Game::~Game() {
-
+    if (player) {
+        delete player;
+    }
 };

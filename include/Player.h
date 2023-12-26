8558448259b6b286c18e58b953ce9a3d14e98638
fanisvl <fanis.vlahogiannis@gmail.com>
2023-12-26 20:48:22 +0200

    add player

--- include/Player.h
+++ include/Player.h
@@ -1,7 +1,13 @@
 #pragma once
 #include "GameObject.h"
+#include "config.h"
 
 class Player : public GameObject {
+
+    float posX = CANVAS_WIDTH / 2;
+    float posY = CANVAS_HEIGHT / 2;
+    float speed = 10.0f;
+
 public:
     void update() override;
     void draw() override;
--- src/Player.cpp
+++ src/Player.cpp
@@ -1,10 +1,32 @@
+#include <cmath>
 #include "Player.h"
+#include "graphics.h"
 
 void Player::update() {
 
+    if (graphics::getKeyState(graphics::SCANCODE_A)) {
+        posX -= speed * graphics::getDeltaTime() / 10.0f;
+    }
+
+    if (graphics::getKeyState(graphics::SCANCODE_D)) {
+        posX += speed * graphics::getDeltaTime() / 10.0f;
+    }
+
+    if (graphics::getKeyState(graphics::SCANCODE_W)) {
+        posY -= speed * graphics::getDeltaTime() / 10.0f ;
+    }
+
+    if (graphics::getKeyState(graphics::SCANCODE_S)) {
+        posY += speed * graphics::getDeltaTime() / 10.0f;
+    }
 }
 
 void Player::draw() {
+
+    graphics::Brush br;
+    br.texture = std::string(ASSET_PATH) + "ghost.png";
+    br.outline_opacity = 0.0f;
+    graphics::drawRect(posX, posY, 100, 100, br);
 
 }
 

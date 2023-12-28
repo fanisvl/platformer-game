ee2e41f2d686fd681bbc1a992fda5eb58162bd8b
fanisvl <fanis.vlahogiannis@gmail.com>
2023-12-28 15:26:24 +0200

    header done

--- include/GameObject.h
+++ include/GameObject.h
@@ -12,7 +12,7 @@ protected:
     bool mActive = true;
 
 public:
-    virtual void update() = 0;
+    virtual void update(float dt) = 0;
     virtual void draw() = 0;
     virtual void init() = 0;
 
--- include/Player.h
+++ include/Player.h
@@ -1,15 +1,19 @@
 #pragma once
 #include "GameObject.h"
+#include "graphics.h"
+#include "box.h"
 #include "config.h"
 
-class Player : public GameObject {
+class Player : public GameObject, public Box {
 
+    graphics::Brush mBrushPlayer;
     float posX = CANVAS_WIDTH / 2.0f;
     float posY = CANVAS_HEIGHT / 2.0f;
     float speed = 10.0f;
 
 public:
-    void update() override;
+    Player(std::string name) : GameObject(name) {}
+    void update(float dt) override;
     void draw() override;
     void init() override;
 };

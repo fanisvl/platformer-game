747730ab629d877e27592de6b851abe37831b2cb
fanisvl <fanis.vlahogiannis@gmail.com>
2024-01-01 19:13:36 +0200

    add const to str

--- include/Block.h
+++ include/Block.h
@@ -14,5 +14,5 @@ public:
     void update(float dt) override;
     void draw() override;
     void init() override;
-    Block(float x, float y, float w, float h, std::string& assetName);
+    Block(float x, float y, float w, float h, const std::string& assetName);
 };

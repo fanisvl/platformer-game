8ed5b763c2351267a08444d52766775a8cd2fc26
fanisvl <fanis.vlahogiannis@gmail.com>
2024-01-04 12:53:31 +0200

    remove global vars

--- include/GameState.h
+++ include/GameState.h
@@ -16,9 +16,6 @@ private:
 
 public:
     bool mDebugging = false;
-    // TODO: Make player a friendly class
-    float mGlobalOffsetX = 0.0f;
-    float mGlobalOffsetY = 0.0f;
 
     void update(float dt);
     void draw();

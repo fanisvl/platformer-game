161db95fd049f019bfd30ffd3388553888e9d536
fanisvl <fanis.vlahogiannis@gmail.com>
2024-01-05 19:19:08 +0200

    move assets folder

--- include/config.h
+++ include/config.h
@@ -2,7 +2,7 @@
 
 // Asset path is relative to where the project is ran from.
 // This asset path assumes that you execute from the top project folder.
-#define ASSET_PATH "bin/assets/"
+#define ASSET_PATH "assets/"
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 800
 #define CANVAS_WIDTH 6

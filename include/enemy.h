1c324e630343ccd680f94994899aff0ffa98633a
fanisvl <fanis.vlahogiannis@gmail.com>
2024-01-07 15:37:38 +0200

    enemy header file

--- include/enemy.h
+++ include/enemy.h
@@ -5,4 +5,10 @@
 class Enemy : public GameObject, public Box {
 	
 	std::string asset_path;
+	const float m_vx = 5.0f;
+public:
+	void update(float dt) override;
+	void draw() override;
+	void init() override;
+	Enemy(std::string name) : GameObject(name) {}
 };

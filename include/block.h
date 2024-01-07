f21abbadab64536142a07166e64bb9d899c40bcd
fanisvl <fanis.vlahogiannis@gmail.com>
2024-01-07 15:57:42 +0200

    add tood

--- include/block.h
+++ include/block.h
@@ -14,5 +14,6 @@ public:
     void update(float dt) override;
     void draw() override;
     void init() override;
+    // TODO: If GameObject inherits from Box, call GameObject consturctor. + move m_asset_name to GameObject.
     Block(float x, float y, float w, float h, const std::string& assetName);
 };

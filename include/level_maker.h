bf92e1a56c76866c1fd8ce538bf573a60df97fe2
fanisvl <fanis.vlahogiannis@gmail.com>
2024-01-19 21:17:42 +0200

    mouse draw rect

--- include/level_maker.h
+++ include/level_maker.h
@@ -5,6 +5,9 @@
 class LevelMaker : public GameObject {
 	graphics::MouseState mouse;
 	graphics::Brush mouse_brush;
+	float mouse_canvas_x;
+	float mouse_canvas_y;
+
 public:
 	void update(float ms) override;
 	void draw() override;
--- src/level_maker.cpp
+++ src/level_maker.cpp
@@ -8,18 +8,16 @@ void LevelMaker::update(float ms) {
 	}
 
 	// Assuming mouse.cur_pos_x and mouse.cur_pos_y are pixel coordinates
-	float canvas_x = (mouse.cur_pos_x / static_cast<float>(WINDOW_WIDTH)) * CANVAS_WIDTH;
-	float canvas_y = (mouse.cur_pos_y / static_cast<float>(WINDOW_HEIGHT)) * CANVAS_HEIGHT;
+	mouse_canvas_x = (mouse.cur_pos_x / static_cast<float>(WINDOW_WIDTH)) * CANVAS_WIDTH;
+	mouse_canvas_y = (mouse.cur_pos_y / static_cast<float>(WINDOW_HEIGHT)) * CANVAS_HEIGHT;
 
-	std::cout << "Canvas X: " << canvas_x << std::endl;
-	std::cout << "Canvas Y: " << canvas_y << std::endl;
 
 
 }
 
 void LevelMaker::draw() {
 	mouse_brush.fill_opacity = 1.0f;
-	
+	graphics::drawRect(mouse_canvas_x, mouse_canvas_y, 1.0f, 1.0f, mouse_brush);
 }
 
 void LevelMaker::init() {

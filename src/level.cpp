26448646fe794dcfafc51d5b729df966d49b5116
fanisvl <fanis.vlahogiannis@gmail.com>
2024-01-22 17:18:46 +0200

    comment out music

--- src/level.cpp
+++ src/level.cpp
@@ -82,7 +82,8 @@ void Level::draw()
 
 void Level::init()
 {
-    graphics::playMusic("assets\\music.mp3", 0.5f, true, 0);
+    //graphics::playMusic("assets\\music.mp3", 0.1f, true, 0);
+  
     // Create background
     if (m_background == nullptr) {
         m_background = new Background();

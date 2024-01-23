e4a1d65de3f360d0edb86b4ca9b94199706d6700
fanisvl <fanis.vlahogiannis@gmail.com>
2024-01-23 20:38:05 +0200

    fix spikes not working

--- src/spikes.cpp
+++ src/spikes.cpp
@@ -1,4 +1,6 @@
 #include "spikes.h"
+#include <sstream>
+
 
 void Spikes::update(float dt) {
     StaticObject::update(dt);
@@ -24,4 +26,10 @@ void Spikes::handleCollision(CollisionType type) {
 Spikes::Spikes(float x, float y, float w, float h, const std::string& asset) 
     : StaticObject(x, y, w, h, asset)
 {
+}
+
+std::string Spikes::to_string() const {
+        std::ostringstream oss;
+        oss << "Spikes" << " " << m_pos_x << " " << m_pos_y << " " << m_width << " " << m_height << " " << m_asset_path;
+        return oss.str();
 }
